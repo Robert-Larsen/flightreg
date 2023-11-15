ecc4c956048fd52ec0db99e6e7fea0ef1370f1ed
Magnus Sælensminde <magnus.salensminde@bekk.no>
2023-11-15 17:04:41 +0100

    POC for å lage en epost/passord bruker i firebase og logge inn, kun med en enkel knapp

--- src/components/FlightList.tsx
+++ src/components/FlightList.tsx
@@ -1,56 +1,117 @@
-import {useEffect, useState} from "react";
-import {getFlights} from "../firebase";
-import { QuerySnapshot } from 'firebase/firestore';
-import { Accordion } from '@mantine/core';
-import {Flight} from "../model/Flight";
-import {FlightData} from "../model/FlightData";
+import { useEffect, useState } from "react";
+import { getFlights } from "../firebase";
+import { QuerySnapshot } from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { Accordion, Button } from "@mantine/core";
+import { Flight } from "../model/Flight";
+import { FlightData } from "../model/FlightData";
 
+function login() {
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, "vototan483@bixolabs.com", "vjraolirfalkngr")
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log("User logged in");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
 
-function FlightList () {
-    const [flights, setFlights] = useState<Flight[]>([]);
+function createUser() {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(
+    auth,
+    "vototan483@bixolabs.com",
+    "vjraolirfalkngr"
+  )
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      console.log("Created user");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
+}
 
-    useEffect(() => {
-        async function fetchFlights() {
-            try {
-                const querySnapshot: QuerySnapshot = await getFlights();
+function FlightList() {
+  const [flights, setFlights] = useState<Flight[]>([]);
 
-                const flights: Flight[] = [];
-                querySnapshot.forEach((doc) => {
-                    const data: FlightData = doc.data() as FlightData;
-                    flights.push({
-                        id: doc.id,
-                        data
-                    });
-                });
-                setFlights(flights);
-            } catch(error) {
-                console.error('Error fetching flights:', error);
-            }
-        }
-        fetchFlights();
-    }, []);
+  useEffect(() => {
+    async function fetchFlights() {
+      try {
+        const querySnapshot: QuerySnapshot = await getFlights();
 
-    const flightAccordion = flights.map((flight) => (
-        <Accordion.Item key={flight.id} value={flight.data.flight + flight.data.departure}>
-            <Accordion.Control>{`${flight.data.flight} ${flight.data.departure.toDate().toLocaleString("no-NO")}`}</Accordion.Control>
-            <Accordion.Panel>
-                <strong>Selskap: </strong>{flight.data.airline}<br/>
-                <strong>Flight: </strong>{flight.data.flight}<br/>
-                <strong>Reg: </strong>{flight.data.reg}<br/>
-                <strong>Fra: </strong>{flight.data.from}<br/>
-                <strong>Til: </strong>{flight.data.to}<br/>
-                <strong>Type: </strong>{flight.data.type}<br/>
-                <strong>Avgang: </strong>{flight.data.departure.toDate().toLocaleString("no-NO")}<br/>
-                <strong>Ankomst: </strong>{flight.data.arrival.toDate().toLocaleString("no-NO")}<br/>
-            </Accordion.Panel>
-        </Accordion.Item>
-    ));
+        const flights: Flight[] = [];
+        querySnapshot.forEach((doc) => {
+          const data: FlightData = doc.data() as FlightData;
+          flights.push({
+            id: doc.id,
+            data,
+          });
+        });
+        setFlights(flights);
+      } catch (error) {
+        console.error("Error fetching flights:", error);
+      }
+    }
+    fetchFlights();
+  }, []);
 
-    return (
-        <Accordion>
-            {flightAccordion}
-        </Accordion>
-    );
+  const flightAccordion = flights.map((flight) => (
+    <Accordion.Item
+      key={flight.id}
+      value={flight.data.flight + flight.data.departure}
+    >
+      <Accordion.Control>{`${flight.data.flight} ${flight.data.departure
+        .toDate()
+        .toLocaleString("no-NO")}`}</Accordion.Control>
+      <Accordion.Panel>
+        <strong>Selskap: </strong>
+        {flight.data.airline}
+        <br />
+        <strong>Flight: </strong>
+        {flight.data.flight}
+        <br />
+        <strong>Reg: </strong>
+        {flight.data.reg}
+        <br />
+        <strong>Fra: </strong>
+        {flight.data.from}
+        <br />
+        <strong>Til: </strong>
+        {flight.data.to}
+        <br />
+        <strong>Type: </strong>
+        {flight.data.type}
+        <br />
+        <strong>Avgang: </strong>
+        {flight.data.departure.toDate().toLocaleString("no-NO")}
+        <br />
+        <strong>Ankomst: </strong>
+        {flight.data.arrival.toDate().toLocaleString("no-NO")}
+        <br />
+      </Accordion.Panel>
+    </Accordion.Item>
+  ));
+
+  return (
+    <>
+      <Button onClick={() => login()}></Button>
+      <Accordion>{flightAccordion}</Accordion>
+    </>
+  );
 }
 
 export default FlightList;
