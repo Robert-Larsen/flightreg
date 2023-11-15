bc686f8b658c7146f5c05731e0f971e715d1e0ff
aslak.sheker.mkadmi <aslak.sheker.mkadmi@vipps.no>
2023-11-15 16:41:25 +0100

    Autocomplete på fra til vha ICAO kode

--- src/components/FlightRegistration.tsx
+++ src/components/FlightRegistration.tsx
@@ -1,16 +1,17 @@
-import {Button, Container, TextInput} from "@mantine/core";
+import {Autocomplete, Button, Container, TextInput} from "@mantine/core";
 import {DateTimePicker} from "@mantine/dates";
-
+import * as data from './airports.json';
 
 function FlightRegistration () {
-
+    const icaos = [...new Set(data.default.map(a => a.ICAO))];
+    console.log(icaos)
     return (
         <Container>
             <TextInput label="Selskap" placeholder="Selskap"/>
             <TextInput label="Flight" placeholder="Flight"/>
             <TextInput label="Reg" placeholder="Reg"/>
-            <TextInput label="Fra" placeholder="Fra"/>
-            <TextInput label="Til" placeholder="Til"/>
+            <Autocomplete data={icaos} limit={10} label="Fra" placeholder="Fra"/>
+            <Autocomplete data={icaos} limit={10} label="Til" placeholder="Til"/>
             <TextInput label="Type" placeholder="Type"/>
             <DateTimePicker label="Avgang" placeholder="Avgang"></DateTimePicker>
             <DateTimePicker label="Ankomst" placeholder="Ankomst"></DateTimePicker>
