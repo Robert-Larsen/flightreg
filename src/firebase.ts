d75e6c75559b8f1cbeff3a34778e478c81c9358f
Johannes Kvamme <johannes.kvamme@bekk.no>
2023-11-15 17:17:21 +0100

    :passport_control: Working google signin

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,27 +1,27 @@
-import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 import {
-    getFirestore,
-    collection,
-    getDocs,
-    query,
-    orderBy
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  orderBy,
 } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_APP_ID
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(firebaseConfig);
+export const configuredFirebase = firebase;
 const db = getFirestore(app);
 
-
 export async function getFlights() {
-    const fligthsRef = collection(db, "flights");
-    const q = query(fligthsRef, orderBy("departure", "desc"))
-    return await getDocs(q);
+  const fligthsRef = collection(db, "flights");
+  const q = query(fligthsRef, orderBy("departure", "desc"));
+  return await getDocs(q);
 }
