43f5c02b40a56082f59da8e2bb104f195e442c9c
Vegard Veiset <veiset@gmail.com>
2023-11-15 16:35:51 +0100

    Add flight warning about same flight

--- src/components/FlightRegistration.tsx
+++ src/components/FlightRegistration.tsx
@@ -1,14 +1,36 @@
 import {Button, Container, TextInput} from "@mantine/core";
 import {DateTimePicker} from "@mantine/dates";
+import {useEffect, useState} from "react";
+import {Flight} from "../model/Flight.ts";
+import {fetchFlights} from "../api/flightFetch.ts";
 
 
 function FlightRegistration () {
+
+  const [flights, setFlights] = useState<Flight[]>([]);
+  const [uniqueFlights, setUniqueFlights] = useState<string[]>([]);
+  const [warning, setWarning] = useState(false);
+
+  useEffect(() => {
+    fetchFlights(setFlights);
+  }, []);
+
+  useEffect(() => {
+    const unique = flights.map((f) => f.data.reg)
+      .filter((item, i, ar) => ar.indexOf(item) === i);
+    setUniqueFlights(unique);
+  }, [flights]);
+
 
     return (
         <Container>
             <TextInput label="Selskap" placeholder="Selskap"/>
             <TextInput label="Flight" placeholder="Flight"/>
-            <TextInput label="Reg" placeholder="Reg"/>
+            <TextInput label="Reg" placeholder="Reg"
+                       onChange={(e) => setWarning(uniqueFlights.includes(e.target.value)) }
+                       onBlur={(e) => setWarning(uniqueFlights.includes(e.target.value)) }
+            />
+            {warning && <div>Du har flydd dette flyet før!</div>}
             <TextInput label="Fra" placeholder="Fra"/>
             <TextInput label="Til" placeholder="Til"/>
             <TextInput label="Type" placeholder="Type"/>
